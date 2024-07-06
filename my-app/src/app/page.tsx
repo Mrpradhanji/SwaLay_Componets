222b06cb314c3c8886115c46d498cd8ad1594b46
Mrpradhanji <114905691+Mrpradhanji@users.noreply.github.com>
2024-07-06 13:39:17 +0530

    Done with Validation

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
-
+import SocialLinsForm from './components/SocialLinksForm'
 export default function Home() {
   return (
     
       <div>
-        <p>ha
-      
-        </p>
+      <SocialLinsForm/>
       </div>
     
   );
