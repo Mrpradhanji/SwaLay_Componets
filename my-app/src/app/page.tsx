4b87f0de7a599b4d910e63d56d6d06eaddce071a
Mrpradhanji <114905691+Mrpradhanji@users.noreply.github.com>
2024-07-06 14:46:07 +0530

    Third FOrm Done Used FontAwesomeIcon

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import SocialLinsForm from './components/SocialLinksForm'
+import LinkExistingArtist from './components/LinkExistingArtist'
 export default function Home() {
   return (
     
       <div>
-      <SocialLinsForm/>
+      <LinkExistingArtist/>
       </div>
     
   );
